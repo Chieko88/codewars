ab4db4e51f7d9e6fad0d67c6c39c7085590e9312
Chieko Sato <chiekosato.02@gmail.com>
2023-03-23 09:31:31 +0900

    done

---
+++ src/7_Divide and Conquer.rb
@@ -0,0 +1,18 @@
+# Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers. Return as a number.
+
+def div_con(x)
+  # Code here
+  string_array = x.select { |element| element.is_a?(String) }
+  string = 0
+  string_array.each do |n|
+    string += n.to_i
+  end
+
+  integer_array = x.select { |element| element.is_a?(Integer) }
+  integer = 0
+  integer_array.each do |n|
+    integer += n
+  end
+
+  return integer - string
+end
